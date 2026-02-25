4abb6847956d33602702bfceca30b39e4910a112
ntvibe <hakerche@gmail.com>
2026-02-25 13:01:26 +0000

    Set site URL for GitHub Pages project site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,4 @@
-import { defineConfig } from 'astro/config';
-
 export default defineConfig({
-  site: 'https://ntvibe.github.io/monovfx-portfolio/',
+  site: 'https://ntvibe.github.io',
+  base: '/monovfx-portfolio',
 });
